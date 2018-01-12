bcf8b62e6e407cd677fe4d64d6ff465969bf93e9
Sevgin Mustafov <sevgin95@gmail.com>
2018-01-12 22:47:49 +0200

    Provide Tests

--- test/remittance-tests.js
+++ test/remittance-tests.js
@@ -1,7 +1,8 @@
-var Remittance = artifacts.require('./Remittance.sol');
+const Remittance = artifacts.require('./Remittance.sol');
+const expectThrow = require('./util.js').expectThrow;
 
 contract('Remittance', function(accounts) {
-    var remittance;
+    var remittanceInstance;
     const owner = accounts[0];  // alice
     const recipient = accounts[1];  // bob
     const exchange = accounts[2];  // carol
@@ -11,133 +12,154 @@ contract('Remittance', function(accounts) {
     var password = "0x9d1437de893f788f85e064b803382800b046563e0a6f0c208e11a86a26aace4f";
     
     var amount = 1000000000000000000;
-    var durationLimit = 10000;
-    var duration = 100;
+    var durationLimit = 1000000000;
+    var duration = 10000000;
 
-    describe('Convert and Send Funds', function() {
-        it('Exchange can convert and send money', function() {
-            var remittance;
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.convertAndSend(firstPasswordHash, secondPasswordHash, owner, { from: exchange});
-            }).then(function(res) {
-                assert.equal(res, true);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
 
-        it("Recipient cannot convert and send money", function () {
-            var remittance;
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.convertAndSend(firstPasswordHash, secondPasswordHash, owner, { from: recipient});
-            }).then(function (res) {
-                assert.equal(res, false);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
+    describe("create remittance", () => {
 
-        it('Owner can convert and send money', function() {
-            var remittance;
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.convertAndSend(firstPasswordHash, secondPasswordHash, owner, { from: owner});
-            }).then(function(res) {
-                assert.equal(res, false);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
+        beforeEach(async function () {
+			remittanceInstance = await Remittance.new({
+				from: owner
+			});
+		})
 
-        it('Exchange cannot convert and send money with wrong password', function() {
-            var remittance;
-            var wrongPassword = "WrongOne"
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.convertAndSend(wrongPassword, secondPasswordHash, owner, { from: exchange});
-            }).then(function(res) {
-                assert.equal(res, false);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
+		it("should create remittance correctly by the owner", async function () {
+			remittanceInstance.createRemittance(exchange, password, durationLimit, {
+				value: amount,
+				from: owner
+			}) 
+        })
 
-        it('Exchange cannot convert and send money with wrong owner address', function() {
-            var remittance;
-            var wrongPassword = "WrongOne"
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.convertAndSend(wrongPassword, secondPasswordHash, recipient, { from: exchange});
-            }).then(function(res) {
-                assert.equal(res, false);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
+        it("should throw on zero amount", async function () {
+			const amountWei = 0;
+			await expectThrow(remittanceInstance.createRemittance(exchange, password, durationLimit, {
+				value: amountWei,
+				from: owner
+			}))
+        })
+        
+        it("should throw on null exchange address", async function () {
+			const exchange = 0x0;
+			await expectThrow(remittanceInstance.createRemittance(exchange, password, durationLimit, {
+				value: amount,
+				from: owner
+			}))
+		})
     });
-
-    // Refund function
     
-    describe('Refund', function() {
-        it('Owner refund successfully', function() {
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.convertAndSend(password, { from: owner });
-            }).then(function(res) {
-                assert.equal(res, true);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
 
-        it('Owner wrong password', function() {
-            var wrongPassword = "wrongPass";
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.convertAndSend(wrongPassword, { from: owner });
-            }).then(function(res) {
-                assert.equal(res, true);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
+    describe("convert and send remittance", () => {
+
+        beforeEach(async function () {
+			remittanceInstance = await Remittance.new({
+				from: owner
+            });
+            
+            remittanceInstance.createRemittance(exchange, password, durationLimit, {
+				value: amount,
+				from: owner
+			}) 
+		})
+		
+        it("should throw on wrong msg.sender, exchange can only call the function", async function () {
+			await expectThrow(remittanceInstance.convertAndSend(firstPasswordHash, secondPasswordHash, owner, {
+				from: owner
+			})) 
+        })
+
+        it("should throw on wrong msg.sender, recipient can only call the function", async function () {
+			await expectThrow(remittanceInstance.convertAndSend(firstPasswordHash, secondPasswordHash, owner, {
+				from: recipient
+			})) 
+        })
+        
+        it("should throw on null owner address", async function () {
+			const owner = 0x0;
+			await expectThrow(remittanceInstance.convertAndSend(firstPasswordHash, secondPasswordHash, owner, {
+				from: exchange
+			}))
+        })
+
+        it("should be successfully called", async function () {
+			remittanceInstance.convertAndSend(firstPasswordHash, secondPasswordHash, owner, {
+				from: exchange
+			})
+        })
+    })
+
+    describe("kill remittance", () => {
+        beforeEach(async function () {
+            remittanceInstance = await Remittance.new({
+                from: owner
+            })
+
+            remittanceInstance.createRemittance(exchange, password, durationLimit, {
+				value: amount,
+				from: owner
+			}) 
+        })
+        
+        it("should kill the contract successfully", async function () {
+            remittanceInstance.kill({
+                from: owner
+            }) 
+        })
+
+        it("should throw on wrong msg.sender, exchange can only call the function", async function () {
+            await expectThrow(remittanceInstance.kill({
+                from: exchange
+            })) 
+        })
+
+        it("should throw on wrong msg.sender, exchange can only call the function", async function () {
+            await expectThrow(remittanceInstance.kill({
+                from: recipient
+            })) 
+        })
     });
 
-    // Kill function
-  	describe('Kill Contract', function() {
-        it('Owner can kill the contract', function() {
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.kill({ from: owner});
-            }).then(function(res) {
-                assert.equal(res, true);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
+    
+    describe("refund remittance", () => {
+        beforeEach(async function () {
+            remittanceInstance = await Remittance.new({
+                from: owner
+            });
 
-        it('Exchange cannot kill the contact', function() {
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.kill({ from: exchange});
-            }).then(function(res) {
-                assert.equal(res, false);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
+            remittanceInstance.createRemittance(exchange, password, 0, {
+				value: amount,
+				from: owner
+			}) 
+        })
+        
+        it("should throw due to durationLimit", async function () {
+            remittanceInstance.createRemittance(exchange, password, durationLimit, {
+				value: amount,
+				from: owner
+            }) 
+            
+            await expectThrow(remittanceInstance.refund(password, {
+                from: owner
+            }))
+        })
 
-        it('Recipient cannot kill the contact', function() {
-            return Remittance.deployed().then(function (instance) {
-                remittance = instance;
-                return remittance.kill({ from: recipient});
-            }).then(function(res) {
-                assert.equal(res, false);
-            }).catch(function(error) {
-                // Error
-            });	
-        });
+        it("should throw owner can refund not exchange", async function () {
+            await expectThrow(remittanceInstance.refund(password, {
+                from: exchange
+            }))
+        })
+
+        it("should throw owner can refund not recipient", async function () {
+            await expectThrow(remittanceInstance.refund(password, {
+                from: recipient
+            }))
+        })
+
+        it("should refund successfully", async function () {
+            remittanceInstance.refund(password, {
+                from: exchange
+            })
+        })
+        
     });
 });
